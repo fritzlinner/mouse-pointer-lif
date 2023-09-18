91c1cdb42e3c55327617b0c818b65e5d88d449d3
fritzlinner <114403111+fritzlinner@users.noreply.github.com>
2023-09-18 14:37:41 +0000

    mousefollowing

---
+++ tracking.js
@@ -0,0 +1,9 @@
+// console.log(document.body)
+// document.body.onmousemove =
+//     (ev) => {
+//         console.log(ev)
+//     }
+
+    document.body.addEventListener('mousemove' , (ev) => {
+console.log(ev)
+    })
