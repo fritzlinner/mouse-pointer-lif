e19c9659381e9805c42e7de11a2636cf145b7551
fritzlinner <114403111+fritzlinner@users.noreply.github.com>
2023-09-25 14:42:08 +0000

    asdfasdfasdf

--- tracking.js
+++ tracking.js
@@ -1,9 +1,32 @@
-// console.log(document.body)
-// document.body.onmousemove =
-//     (ev) => {
-//         console.log(ev)
-//     }
+const position = []
+const dot = documents.getElementById('dot')
+const isRecording = false
+const isReplaying = false
+document.body.addEventListener('mousemove', (ev) => {
+    console.log(ev)
+ if (isRecording) {
 
-    document.body.addEventListener('mousemove' , (ev) => {
-console.log(ev)
-    })
+ document.getElementById('mousex').textContent = ev.pageX
+    document.getElementById('mousey').textContent = ev.pageX
+    const pos = {
+        x: ev.pageX,
+        y: ev.pageY,
+        t: Date.now()
+    }
+    position.push(pos)}
+})
+document.onkeyup = (ev) => {
+    if (ev.key === 'Enter') {
+        isReplaying = true
+        showFakecursor()
+    }
+}
+const showFakecursor = () => {
+    if (position.lengh >= 2) {
+        const pos = position.shift()
+        dot.style.left = pos.x + 'px'
+        dot.style.top = pos.y + 'px'
+        const dt = position[0].t - pos.t;
+       if (isReplaying) { window.setTimeout(showFakecursor, dt)}
+    }
+}
